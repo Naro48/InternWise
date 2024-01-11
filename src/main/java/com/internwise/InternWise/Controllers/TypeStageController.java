0991c60369327e23364aa586b29a218156fa16e1
Naro48 <nassim_feddouli@um5.ac.ma>
2024-01-11 19:48:41 +0100

    CORS configuration

--- src/main/java/com/internwise/InternWise/Controllers/TypeStageController.java
+++ src/main/java/com/internwise/InternWise/Controllers/TypeStageController.java
@@ -3,12 +3,14 @@ package com.internwise.InternWise.Controllers;
 import com.internwise.InternWise.Service.TypeStageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/typestage")
+@CrossOrigin("http://localhost:4200/")
 public class TypeStageController {
     private final TypeStageImpl typeStage;
 
